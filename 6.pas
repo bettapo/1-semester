efc0e42634969bcbb31962271baceb113d3c9943
betLomo <betta.po@yandex.ru>
2017-10-20 00:09:03 +0300

    Update 6.pas

--- 6.pas
+++ 6.pas
@@ -14,62 +14,68 @@ type  pointer = ^element;
       end;
 ff_rec = file of element;
 
-var head, p {A pointer to the first element}
-    //x     {Additional pointer to create the next list item}
-        : pointer;
-    //quantity, mark, i: integer;
-    //srname: string[255];
+var head, honorsHead, loserHead, otherHead, p : pointer;
+    digitSurname : string[255];        {Variables to create the list}
+    digitMark1, digitMark2 : integer;  {Variables to create the list}
 
-Procedure Init(Var u : pointer);
-Var
-  x : pointer;
-  digitSurname : string[255];
-  digitMark1, digitMark2: integer;
+Function Push(var first : pointer; srname: string; mark1, mark2 : integer ) : pointer;
+var x : pointer;
 begin
-  writeln('Enter the list');
-  //u := Nil; {The list is empty}
-  writeln ('Enter the list items. To finish entering, type *');
-  readln(digitSurname, digitMark1, digitMark2);
-  //readln;
-  if (digitSurname <> '*') then {Generate and insert the first element of the list}
-      begin
-        //New(x);
-        u^.Next := Nil;
-        u^.surname := digitSurname;
-        u^.mark1 := digitMark1;
-        u^.mark2 := digitMark2;
-        x := u;
-        readln (digitSurname);
-        readln (digitMark1);
-        readln (digitMark2);
-        while (digitSurname <> '*') do
-          begin
-            New(x^.Next); {Create and insert an element at the end of the list}
-            x := x^.Next;
-            x^.Next := Nil;
-            x^.surname := digitSurname;
-            x^.mark1 := digitMark1;
-            x^.mark2 := digitMark2;
-            readln (digitSurname);
-            readln (digitMark1);
-            readln (digitMark2);
-          end;
-      end;
-End;
+     New(x); {Creating a pointer to a new item}
+     x^.Next := first;
+     x^.surname := srname;
+     x^.mark1 := mark1;
+     x^.mark2 := mark2;
+     Push := x;
+end;
 
-
-
+Procedure Output (p : pointer);
 begin
-     New(head);
-     Init(head);
-
-     {List output in the console}
-     p := head;
      while p<>Nil do
      begin
           writeln(p^.surname, ' ', p^.mark1, ' ', p^.mark2);
           p := p^.Next;
      end;
-     {}
+     writeln;
+end;
 
+begin
+  {Initialize lists}
+  head := Nil;
+  honorsHead := Nil;
+  loserHead := Nil;
+  otherHead := Nil;
+  {}
+
+  writeln ('Enter the list items. To finish entering, type *');
+  readln(digitSurname);
+  while (digitSurname<>'*') do
+    begin
+      readln(digitMark1, digitMark2);
+      head := Push(head, digitSurname, digitMark1, digitMark2);
+      if(digitMark1 = 5) and (digitMark2 = 5) then
+          honorsHead := Push(honorsHead, digitSurname, digitMark1, digitMark2)
+      else if (digitMark1 = 2) or (digitMark2 = 2) or (digitMark1 = 1) or (digitMark2 = 1) then
+              loserHead := Push(loserHead, digitSurname, digitMark1, digitMark2)
+           else otherHead := Push(otherHead, digitSurname, digitMark1, digitMark2);
+      readln(digitSurname);
+    end;
+
+  {General list output in the console}
+     writeln('General list: ');
+     Output(head);
+
+  {List of honors pupils output in the console}
+     writeln('List of honors pupils: ');
+     Output(honorsHead);
+
+  {List of losers output in the console}
+     writeln('List of losers: ');
+     Output(loserHead);
+
+  {List of others output in the console}
+     writeln('List of others: ');
+     Output(otherHead);
+
+     readln;
 end.
